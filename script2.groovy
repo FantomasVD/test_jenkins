dc10ad95a70d4fc6d9c8c42363a76327d9966933
Stanislav Dvorkin <dvorkin.stanislav@gmail.com>
2021-12-28 08:47:22 +0300

    add post stage

--- script2.groovy
+++ script2.groovy
@@ -5,11 +5,6 @@ def buildJar() {
 
 def buildImage() {
   echo "building the docker image ..."
-  withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    sh 'docker build -t svizist104/my-repo:jma-2.0 .'
-    sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh 'docker push svizist104/my-repo:jma-2.0'
-  }
 }
 
 def deployApp() {
